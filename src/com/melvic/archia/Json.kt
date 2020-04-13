08b7fb6f68f9c4266fbb6a0e5fa4c07de9c135c8
Melvic <melvic.ybanez2014@gmail.com>
2020-04-13 23:34:28 +0800

    json string output

--- src/com/melvic/archia/Json.kt
+++ src/com/melvic/archia/Json.kt
@@ -1,9 +1,9 @@
 package com.melvic.archia
 
 sealed class JsonValue {
-    data class JsonString(val str: String) : JsonValue()
-    data class JsonNumber(val num: Number) : JsonValue()
-    data class JsonBoolean(val bool: Boolean) : JsonValue()
+    data class JsonString(val value: String) : JsonValue()
+    data class JsonNumber(val value: Number) : JsonValue()
+    data class JsonBoolean(val value: Boolean) : JsonValue()
     object JsonNull : JsonValue()
     class JsonObject(val entries: Map<String, JsonValue>) : JsonValue()
     class JsonArray(val items: List<JsonValue>) : JsonValue()
