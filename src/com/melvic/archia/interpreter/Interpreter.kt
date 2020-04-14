f6d07ae06739e6f7db29abccf2555cc23ea3fd41
Melvic <melvic.ybanez2014@gmail.com>
2020-04-15 02:24:41 +0800

    Simplify matchings

--- src/com/melvic/archia/interpreter/Interpreter.kt
+++ src/com/melvic/archia/interpreter/Interpreter.kt
@@ -1,6 +1,7 @@
 package com.melvic.archia.interpreter
 
 import com.melvic.archia.Query
+import com.melvic.archia.QueryContext
 import com.melvic.archia.leaf.Term
 import com.melvic.archia.output.JsonArray
 import com.melvic.archia.output.JsonObject
@@ -8,32 +9,21 @@ import com.melvic.archia.output.JsonValue
 import com.melvic.archia.output.json
 
 typealias Evaluation = Result<JsonValue>
-typealias KVEval = Result<Pair<String, JsonValue>>
 
 fun interpret(query: Query): Evaluation {
     fun interpret(query: Query, parent: JsonValue): Evaluation {
-        val output = when (query) {
-            is Term -> interpretTerm(query)
-            else -> Failed(UnknownQuery(query))
-        }
+        val objectOrEmpty = if (parent is JsonObject) parent else json {}
 
-        return when (output) {
-            is Success<*> -> {
-                val (key, value) = output.value()
-                when (parent) {
-                    is JsonObject -> Success(parent { key to value })
-                    is JsonArray -> Success( parent { json { key to value }})
-                    else -> throw Exception("Invalid Json")
-                }
-            }
-            is Failed -> output
+        return when (query) {
+            is Term -> interpretTerm(query, objectOrEmpty)
+            else -> Success(json {})
         }
     }
 
     return interpret(query, json {})
 }
 
-fun interpretTerm(term: Term): KVEval {
+fun interpretTerm(term: Term, parent: JsonObject): Evaluation {
     val field = term.field ?: return Failed(missingField(term::field))
 
     val termFieldOut = json {
@@ -41,7 +31,7 @@ fun interpretTerm(term: Term): KVEval {
         field.boost?.let { "boost" to num(it) }
     }
 
-    val termOut = Pair("term", json { "field" to termFieldOut })
+    val termOut = parent { "term" to json { "field" to termFieldOut } }
 
     return Success(termOut)
 }
