0fc893de8654acbd017698ed6cefa75ff132317d
Melvic Ybanez <melvic@callhandling.co.uk>
2020-04-22 02:22:02 +0800

    Add tests for boosting query

--- src/test/kotlin/BoostingQueryTests.kt
+++ src/test/kotlin/BoostingQueryTests.kt
@@ -2,6 +2,9 @@ import com.melvic.archia.ast.evalQuery
 import com.melvic.archia.interpreter.Failed
 import com.melvic.archia.interpreter.MissingField
 import com.melvic.archia.interpreter.missingFieldCode
+import com.melvic.archia.interpreter.output
+import com.melvic.archia.output.JsonStringOutput
+import com.melvic.archia.output.mapTo
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
@@ -16,6 +19,40 @@ class BoostingQueryTests : BehaviorSpec({
                     MissingField("positive"),
                     MissingField("negative")
                 ))
+            }
+        }
+        `when`("all required fields are supplied") {
+            then("it should return the correct JSON structure") {
+                val output = evalQuery {
+                    boosting {
+                        positive {
+                            term { "text" to "apple" }
+                        }
+                        negative {
+                            term { "text" to "pie tart fruit crumble tree" }
+                        }
+                        negativeBoost = 0.5f
+                    }
+                }.output()
+                output.mapTo(JsonStringOutput).strip() shouldBe """
+                    {
+                        "query": {
+                            "boosting" : {
+                                "positive" : {
+                                    "term" : {
+                                        "text" : "apple"
+                                    }
+                                },
+                                "negative" : {
+                                     "term" : {
+                                         "text" : "pie tart fruit crumble tree"
+                                    }
+                                },
+                                "negative_boost" : 0.5
+                            }
+                        }
+                    }
+                """.strip()
             }
         }
     }
