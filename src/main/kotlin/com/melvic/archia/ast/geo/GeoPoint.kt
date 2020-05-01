b3b051ca80dae0313352d77e77e480e2ac522165
Melvic <melvic.ybanez2014@gmail.com>
2020-05-01 18:58:45 +0800

    Add more tests for the geo distance query

--- src/main/kotlin/com/melvic/archia/ast/geo/GeoPoint.kt
+++ src/main/kotlin/com/melvic/archia/ast/geo/GeoPoint.kt
@@ -7,8 +7,8 @@ sealed class GeoPoint : DecayFieldType
 data class GeoObject(var lat: Number? = null, var lon: Number? = null) : GeoPoint()
 data class GeoString(val lat: Number, val lon: Number) : GeoPoint()
 data class GeoHash(val hash: String) : GeoPoint()
-data class GeoArray(val lat: Double, val lon: Double) : GeoPoint()
-data class GeoWktPoint(val lat: Double, val lon: Double) : GeoPoint()
+data class GeoArray(val lat: Number, val lon: Number) : GeoPoint()
+data class GeoWktPoint(val lat: Number, val lon: Number) : GeoPoint()
 
 data class Bbox(
     val topRight: Double,
--- src/test/kotlin/GeoDistanceQueryTests.kt
+++ src/test/kotlin/GeoDistanceQueryTests.kt
@@ -1,3 +1,4 @@
+import com.melvic.archia.ast.geo.GeoArray
 import io.kotest.core.spec.style.BehaviorSpec
 
 class GeoDistanceQueryTests : BehaviorSpec({
@@ -41,6 +42,23 @@ class GeoDistanceQueryTests : BehaviorSpec({
                             }
                         }
                     """.trimIndent()
+                }
+            }
+        }
+        `when`("the provided geo point isn't a geo object") {
+            then("the result shouldn't contain lat and lon properties") {
+                assert {
+                    query {
+                        bool {
+                            must { matchAll{} }
+                            filter {
+                                geoDistance {
+                                    distance = 12.km()
+                                    "pin.location" to GeoArray(-70, 40)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
