0d7f29b71fb6d13e1482d3eb8b62fcd2f3841a51
Melvic <melvic.ybanez2014@gmail.com>
2020-05-03 23:44:15 +0800

    Add test for multi-level nested query

--- src/main/kotlin/com/melvic/archia/ast/joining/NestedQuery.kt
+++ src/main/kotlin/com/melvic/archia/ast/joining/NestedQuery.kt
@@ -1,13 +1,20 @@
 package com.melvic.archia.ast.joining
 
+import com.melvic.archia.ast.BuilderHelper
 import com.melvic.archia.ast.Clause
+import com.melvic.archia.ast.ClauseBuilder
+import com.melvic.archia.ast.Init
 import kotlin.reflect.KProperty
 
-class NestedQuery : Clause() {
+class NestedQuery : Clause(), BuilderHelper {
     var path: String by parameters
     var query: Clause by parameters
     var scoreMode: ScoreMode by parameters
     var ignoreUnmapped: Boolean by parameters
+
+    fun query(init: Init<ClauseBuilder>) {
+        setClause(init) { query = it }
+    }
 
     override val requiredParams: List<KProperty<Any>>
         get() = listOf(::path, ::query)
--- src/test/kotlin/NestedQueryTests.kt
+++ src/test/kotlin/NestedQueryTests.kt
@@ -16,5 +16,85 @@ class NestedQueryTests : FeatureSpec({
                 errors = listOf(MissingField("path"), MissingField("query"))
             }
         }
+        scenario("should return correct JSON if all required fields are provided") {
+            assert {
+                query {
+                    nested {
+                        path = "obj1"
+                        query {
+                            bool {
+                                must {
+                                    match { "obj1.name" to "blue".es() }
+                                    range { "obj1.count" { gt = 5.es() } }
+                                }
+                            }
+                        }
+                        scoreMode = ScoreMode.AVG
+                    }
+                }
+                expected = """
+                    {
+                        "query":  {
+                            "nested" : {
+                                "path" : "obj1",
+                                "query" : {
+                                    "bool" : {
+                                        "must" : [
+                                        { "match" : {"obj1.name" : "blue"} },
+                                        { "range" : {"obj1.count" : {"gt" : 5}} }
+                                        ]
+                                    }
+                                },
+                                "score_mode" : "avg"
+                            }
+                        }
+                    }
+                """.trimIndent()
+            }
+        }
+        scenario("should support multi-level nesting") {
+            assert {
+                query {
+                    nested {
+                        path = "driver"
+                        query {
+                            nested {
+                                path = "driver.vehicle"
+                                query {
+                                    bool {
+                                        must {
+                                            match { "driver.vehicle.make" to "Powell Motors".es() }
+                                            match { "driver.vehicle.model" to "Canyonero".es() }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                expected = """
+                    {
+                        "query" : {
+                            "nested" : {
+                                "path" : "driver",
+                                "query" : {
+                                    "nested" : {
+                                        "path" :  "driver.vehicle",
+                                        "query" :  {
+                                            "bool" : {
+                                                "must" : [
+                                                    { "match" : { "driver.vehicle.make" : "Powell Motors" } },
+                                                    { "match" : { "driver.vehicle.model" : "Canyonero" } }
+                                                ]
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                """.trimIndent()
+            }
+        }
     }
 })
